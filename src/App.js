aa3e43f8c4aaa49a5ae06eaf0f840a450c8cd0a5
nhat416 <nhat.tran416@icloud.com>
2024-02-22 17:39:09 -0500

    Update demo version text in App.js
    Updated the displayed text to 'WeCloud DevOps - Nhat Demo - version 17' in App.js file. This alteration ensures consistency and accuracy in displaying the correct current version of the application to the users.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WeCloud DevOps - Nhat Demo - version 16
+          WeCloud DevOps - Nhat Demo - version 17
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
